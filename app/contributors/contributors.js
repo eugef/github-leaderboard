cacdd2e478ee4314ed602abeb3dbae77a2eb1d37
Eugene Fidelin <eugene.fidelin@gmail.com>
2015-02-27 13:05:27 +0100

    Move range toolbar to the main controller

--- app/contributors/contributors.js
+++ app/contributors/contributors.js
@@ -12,50 +12,24 @@ angular.module('myApp.controller.contributors', ['ngRoute'])
     .controller('ContributorsCtrl', ['$scope', 'Github', 'config', 'Leaderboard',
         function ($scope, Github, config, Leaderboard) {
             $scope.leaderboard = {};
-            $scope.weeksOffset = 1;
-            $scope.startWeek = null;
-            $scope.endWeek = null;
 
-            $scope.refresh = function() {
+            $scope.$on('refresh', function() {
                 $scope.updateLeaderboard(true);
-            };
+            });
 
-            $scope.setRange = function(weeksOffset) {
-                if ($scope.weeksOffset != weeksOffset) {
-                    $scope.weeksOffset = weeksOffset;
-                    $scope.updateLeaderboard();
-                }
-            };
-
-            $scope.isRange = function(weeksOffset) {
-                return $scope.weeksOffset == weeksOffset;
-            };
+            $scope.$on('setRange', function() {
+                $scope.updateLeaderboard();
+            });
 
             $scope.updateLeaderboard = function(forceUpdate) {
                 forceUpdate = forceUpdate || false;
 
-                var startWeek = startOfTheWeek($scope.weeksOffset);
-                $scope.startWeek = startWeek * 1000;
-                $scope.endWeek = endOfTheWeek() * 1000;
-
                 $scope.$parent.progress.current = 0;
                 
                 for (var i = 0; i < config.projects.length; i++) {
-                    loadProjectContributors(config.projects[i], startWeek, forceUpdate);
+                    loadProjectContributors(config.projects[i], $scope.$parent.startWeek, forceUpdate);
                 }
             };
-
-            function offsetDay(offset) {
-                return moment().utcOffset(0).day(offset).hour(0).minute(0).second(0).format('X');
-            }
-            
-            function startOfTheWeek(weekOffset) {
-                return offsetDay(-7 * weekOffset);
-            }
-
-            function endOfTheWeek() {
-                return offsetDay(6);
-            }
 
             function loadProjectContributors(projectName, startWeek, forceUpdate) {
                 Github.contributors(projectName, forceUpdate).then(function(data) {
--- app/main/main.js
+++ app/main/main.js
@@ -8,6 +8,39 @@ angular.module('myApp.controller.main', [])
                 total: config.projects.length,
                 current: 0
             };
+            $scope.weeksOffset = null;
+            $scope.startWeek = null;
+            $scope.endWeek = null;
+
+            function offsetDay(offset) {
+                return moment().utcOffset(0).day(offset).hour(0).minute(0).second(0).format('X');
+            }
+
+            function startOfTheWeek(weekOffset) {
+                return offsetDay(-7 * weekOffset);
+            }
+
+            function endOfTheWeek() {
+                return offsetDay(6);
+            }
+
+            $scope.isRange = function(weeksOffset) {
+                return $scope.weeksOffset == weeksOffset;
+            };
+
+            $scope.setRange = function(weeksOffset) {
+                if ($scope.weeksOffset != weeksOffset) {
+                    $scope.weeksOffset = weeksOffset;
+                    $scope.startWeek = startOfTheWeek(weeksOffset);
+                    $scope.endWeek = endOfTheWeek();
+
+                    $scope.$broadcast('setRange');
+                }
+            };
+
+            $scope.refresh = function() {
+                $scope.$broadcast('refresh');
+            };
 
             $scope.hasUrl = function(url) {
                 return url == $location.url();
@@ -18,5 +51,7 @@ angular.module('myApp.controller.main', [])
                     if (item[prop] > val) return true;
                 }
             };
+
+            $scope.setRange(1);
         }
     ]);
